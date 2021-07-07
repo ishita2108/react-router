2d19f379831a59be20b042cbc9dfa32b241841f5
ishita2108 <iwinner21@gmail.com>
2021-07-07 15:54:34 +0530

    routing

---
+++ ika/src/App.js
@@ -0,0 +1,30 @@
+import './App.css';
+import Nav from './nav'
+import About from './about'
+import Shop from './shop'
+import Item from './itemdetail'
+import {BrowserRouter as Router, Switch, Route} from 'react-router-dom';
+
+function App() {
+  return (
+    <Router>
+    <div className="App">
+      <Nav />
+      <Switch>
+        <Route path="/" exact component={Home}/>
+        <Route path="/about" component={About}/>
+        <Route path="/shop" exact component={Shop}/>
+        <Route path="/shop/:id" component={Item}/>
+      </Switch>
+    </div>
+    </Router>
+  );
+}
+
+const Home = () =>(
+  <div>
+    <h1>Home Page</h1>
+  </div>
+)
+
+export default App;
